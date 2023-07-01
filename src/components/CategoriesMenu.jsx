f3d2fe07b1e2581c5bbc906861ed97b70bcaa5cc
Viviana Benitez <viviana.bernitez@gmail.com>
2023-07-01 15:25:37 -0500

    categories section added with just styles

---
+++ src/components/CategoriesMenu.jsx
@@ -0,0 +1,17 @@
+import "../style/categoriesMenu.css";
+import React from "react";
+
+const CategoriesMenu = () => {
+  return (
+    <div className="categoriesMenu">
+      <ul>
+        <li>Electronics</li>
+        <li>Jewelery</li>
+        <li>Men's clothing</li>
+        <li>Women's clothing</li>
+      </ul>
+    </div>
+  );
+};
+
+export { CategoriesMenu };

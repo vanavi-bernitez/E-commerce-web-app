b6671335ee67be6907e870d009d99bbf80c976e3
Viviana Benitez <viviana.bernitez@gmail.com>
2023-07-08 10:10:18 -0500

    fixed modal style

--- src/components/ProductModal.jsx
+++ src/components/ProductModal.jsx
@@ -18,17 +18,47 @@ const ProductModal = ({
 
   return (
     <ReactModal
-      className="seeMoreModal"
-      overlayClassName="overlaySeeMoreModal"
       isOpen={isSeeMoreClicked}
+      onRequestClose={handleSeeMoreClick}
+      style={{
+        overlay: {
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          backgroundColor: "rgba(255, 255, 255, 0.9)",
+        },
+        content: {
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          border: "1px solid #ccc",
+          background: "#fff",
+          overflow: "auto",
+          WebkitOverflowScrolling: "touch",
+          borderRadius: "2em",
+          outline: "none",
+          padding: "20px",
+          width: "60vw",
+          height: "50vh",
+        },
+      }}
     >
       <button onClick={handleSeeMoreClick}>x</button>
-      <div className="imgModalContainer">
-        <img src={product.image} alt="e-commerce product" />
+      <div className="productContainer">
+        <div className="imgModalContainer">
+          <img src={product.image} alt="e-commerce product" />
+        </div>
+        <div className="descriptionModalContainer">
+          <h3>{product.title}</h3>
+          <p className="rightAlign">{product.description}</p>
+          <button className="rightAlign" onClick={handleAddClick}>
+            Add
+          </button>
+        </div>
       </div>
-      <p>{product.title}</p>
-      <p>{product.description}</p>
-      <button onClick={handleAddClick}>Add</button>
     </ReactModal>
   );
 };
